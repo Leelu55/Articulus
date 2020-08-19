62db7e1de9434d450dde09e417c11ab133c696eb
Maria Yalpani <maria@yalpani.de>
2020-08-19 10:54:23 +0200

    use useState to iterate over words array and speak word on button click

--- components/Word.tsx
+++ components/Word.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
 
 import model from "../model/model.json";
 import Tts from 'react-native-tts';
 
 export default function Word() {
-  const value = model.words[0].value;
+  const [ wordIndex, setWordIndex ] = useState(0);
+  const value = model.words[wordIndex].value;
+
   Tts.speak(value);
+  //alert(model.words.length);
 
   return (
     <View>
       <Text>Word Component</Text>
       <Text>{value}</Text>
-
+      <Button title="next" onPress= {() => {if (wordIndex < model.words.length - 1) setWordIndex(wordIndex => wordIndex+1)}}></Button>
     </View>
   );
 }
