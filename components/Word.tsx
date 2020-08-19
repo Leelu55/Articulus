3848e4d5f7d3b8a37572a00ff5402454416b14f3
Maria Yalpani <maria@yalpani.de>
2020-08-19 10:25:55 +0200

    use "react-native-tts" text to speech library and say first word value.  Declare library for use with TypeScript in decs.t.ds and use in tsconfig.json

--- components/Word.tsx
+++ components/Word.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 import model from "../model/model.json";
+import Tts from 'react-native-tts';
 
 export default function Word() {
+  const value = model.words[0].value;
+  Tts.speak(value);
+
   return (
     <View>
       <Text>Word Component</Text>
-      <Text>{model.words[0].value}</Text>
+      <Text>{value}</Text>
 
     </View>
   );
