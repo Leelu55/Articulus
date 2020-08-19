af3dc1a2d9c15c3169b35f036c96873e9673f991
Maria Yalpani <maria@yalpani.de>
2020-08-19 10:08:49 +0200

    import model.json into Word component and display value of first word from model.json

--- components/Word.tsx
+++ components/Word.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import model from "../model/model.json";
+
 export default function Word() {
   return (
     <View>
       <Text>Word Component</Text>
+      <Text>{model.words[0].value}</Text>
+
     </View>
   );
 }
