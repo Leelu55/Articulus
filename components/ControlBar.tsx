72ba90b5fadbc7966353b3c1a3150cd7dd4461c4
Maria Yalpani <maria@yalpani.de>
2020-09-16 12:15:58 +0200

    cleanup

--- components/ControlBar.tsx
+++ components/ControlBar.tsx
@@ -1,17 +1,12 @@
 import React, {useContext} from 'react';
 import {View, TouchableHighlight} from 'react-native';
 import styles from '../styles/wordStyle';
-import UIStore from '../stores/UIStore';
-import WordsStore from '../stores/WordsStore';
 import {observer} from 'mobx-react';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import LessonStateIndicator from './LessonStateIndicator';
-import { ForwardButton } from './ForwardButton';
+import {ForwardButton} from './ForwardButton';
 
 function ControlBar() {
-  const wordIndex = useContext(UIStore).wordIndex;
-  const wordsLength = useContext(WordsStore).words.length;
-
   return (
     <View style={[styles.viewHorizontal, styles.controlBar]}>
       <ForwardButton />
