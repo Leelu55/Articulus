ff0b7d9507720787e804a14909e8fe889b20e67a
Maria Yalpani <maria@yalpani.de>
2020-10-06 12:01:43 +0200

    refactored selector buttons and bugfix (disable on pause and finish)

--- components/PlayerScreen.tsx
+++ components/PlayerScreen.tsx
@@ -4,18 +4,22 @@ import {View} from 'react-native';
 import {observer} from 'mobx-react';
 
 import '../styles/wordStyle';
-
-import Selector from './Selector';
 import ControlBar from './ControlBar';
 import {Header} from './Header';
 import Word from './Word';
+import styles from '../styles/wordStyle';
+import SelectorButton from './SelectorButton';
 
 function PlayerScreen() {
   return (
     // eslint-disable-next-line react-native/no-inline-styles
     <View style={{flex: 1}}>
       <Header />
-      <Selector />
+      <View style={styles.viewHorizontal}>
+        <SelectorButton articleText="der" />
+        <SelectorButton articleText="die" />
+        <SelectorButton articleText="das" />
+      </View>
       <Word />
       <ControlBar />
     </View>
---
+++ components/SelectorButton.tsx
@@ -0,0 +1,54 @@
+import {observer} from 'mobx-react';
+import React, {useContext} from 'react';
+import {Text, TouchableHighlight} from 'react-native';
+import UIStore, {LessonState} from '../stores/UIStore';
+import WordsStore from '../stores/WordsStore';
+import styles from '../styles/wordStyle';
+import settings from '../libs/settings.json';
+import processAnswer from '../libs/processAnswer';
+import audioVoice from '../libs/audioVoice';
+
+function SelectorButton({articleText}: {articleText: string}) {
+  const uiStore = useContext(UIStore);
+  const wordsStore = useContext(WordsStore);
+  const wordIndex = uiStore.wordIndex;
+  const currentLessonWord = wordsStore.lessonWords[wordIndex];
+
+  const isCorrectArticle =
+    currentLessonWord.answerArticle === currentLessonWord.article;
+
+  const chosenArticle = () =>
+    currentLessonWord.answerArticle === articleText ? true : false;
+
+  const styleDefault = {opacity: 0.5};
+  const styleCorrect = {
+    backgroundColor: settings.colors.correctAnswer,
+    opacity: 1,
+  };
+  const styleWrong = {backgroundColor: settings.colors.wrongAnswer, opacity: 1};
+
+  const styleSelector = () =>
+    chosenArticle() && isCorrectArticle
+      ? styleCorrect
+      : chosenArticle() && !isCorrectArticle
+      ? styleWrong
+      : styleDefault;
+  return (
+    <TouchableHighlight
+      disabled={[
+        LessonState.IsEvaluating,
+        LessonState.IsPaused,
+        LessonState.IsFinished,
+      ].includes(uiStore.lessonState)}
+      style={[styles.articleButton, styleSelector()]}
+      onPress={() => {
+        audioVoice.voiceStop();
+        audioVoice.stopSpeakWord();
+        processAnswer(wordsStore, uiStore, articleText);
+      }}>
+      <Text style={styles.articleButtonText}>{articleText.toUpperCase()}</Text>
+    </TouchableHighlight>
+  );
+}
+
+export default observer(SelectorButton);
