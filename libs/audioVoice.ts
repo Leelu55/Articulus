e2b7f05700514f433c745d77c9737f5488d37fb4
Maria Yalpani <maria@yalpani.de>
2020-11-12 08:30:39 +0100

    Trying to resolve Test runtime error in Tts.speak() by passing the KEY_PARAM_STREAM value

--- libs/audioVoice.ts
+++ libs/audioVoice.ts
@@ -93,9 +93,10 @@ export function voiceStop() {
 export function stopSpeakWord() {
   ttsLibrary.stop();
 }
-
+// solving E/AndroidRuntime: FATAL EXCEPTION: TTS.AudioPlaybackThread
+// https://github.com/ak1394/react-native-tts/issues/121
 export function speakWord(wordValue) {
-  ttsLibrary.speak(wordValue);
+  ttsLibrary.speak(wordValue, {KEY_PARAM_STREAM: 'STREAM_MUSIC'});
 }
 
 export function repeatWord(prefixText, wordValue) {
