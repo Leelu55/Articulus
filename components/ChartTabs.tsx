4feb2d27f0a5d304b887cc35fc28160ea7c9f9a2
Maria Yalpani <maria@yalpani.de>
2020-11-19 10:08:29 +0100

    ChartTabs: removed testarray savedLessons and use real savedLessons from WordsStore to display learning progress

--- components/ChartTabs.tsx
+++ components/ChartTabs.tsx
@@ -5,113 +5,21 @@ import {View, StyleSheet} from 'react-native';
 import {Dimensions} from 'react-native';
 import {TabView, TabBar} from 'react-native-tab-view';
 
-import {SavedLessonType} from '../stores/WordsStore';
 import populateLineChart from '../libs/populateLineChart';
 import WeekChartTab from './ChartTabs/WeekChartTab';
 import MonthChartTab from './ChartTabs/MonthChartTab';
 import YearChartTab from './ChartTabs/YearChartTab';
 import settings from '../libs/settings.json';
-
-const savedLessons: SavedLessonType[] = [
-  {
-    date: new Date('2020-11-06'),
-    countCorrectAnswers: 25,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2020-11-07'),
-    countCorrectAnswers: 25,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2019-11-08'),
-    countCorrectAnswers: 125,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2020-11-09'),
-    countCorrectAnswers: 50,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2020-11-05'),
-    countCorrectAnswers: 7,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2020-09-25'),
-    countCorrectAnswers: 20,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2020-05-24'),
-    countCorrectAnswers: 13,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2020-10-23'),
-    countCorrectAnswers: 25,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-
-  {
-    date: new Date('2020-05-23'),
-    countCorrectAnswers: 25,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2020-10-22'),
-    countCorrectAnswers: 2,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2020-01-21'),
-    countCorrectAnswers: 5,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2020-01-20'),
-    countCorrectAnswers: 6,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-  {
-    date: new Date('2020-01-19'),
-    countCorrectAnswers: 10,
-    countWrongAnswers: 0,
-    words: [],
-    isFinished: true,
-  },
-];
+import WordsStore from '../stores/WordsStore';
+import {useContext} from 'react';
 
 const initialLayout = {width: Dimensions.get('window').width};
 
 function ChartTabs() {
+  const wordsStore = useContext(WordsStore);
+
   //get processed and formatted data for displaying week|year|months
-  const chartData = populateLineChart(savedLessons);
+  const chartData = populateLineChart(wordsStore.savedLessons);
 
   // tab index
   const [index, setIndex] = React.useState(0);
