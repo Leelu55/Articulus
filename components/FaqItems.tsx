13fbce206411102405194e0f0c31dd526c373798
Maria Yalpani <maria@yalpani.de>
2020-12-02 14:06:35 +0100

    specify and implement FaqScreen

--- components/FaqItems.tsx
+++ components/FaqItems.tsx
@@ -1,74 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import sharedStyles from '../styles/sharedStyles';
-import settings from '../libs/settings.json';
+import {View} from 'react-native';
+import GrammarItem from './GrammarItem';
+import {rules} from '../libs/Rules';
 
 function FaqItems() {
   return (
     <View>
       {Object.keys(rules).map((ruleId) => (
-        <View style={styles.wrapper} key={ruleId}>
-          <Text style={[sharedStyles.label, styles.label]}>
-            {rules[ruleId].label}
-          </Text>
-          <Text style={styles.ruleText}>{rules[ruleId].text}</Text>
-          <Text style={styles.exampleText}>{rules[ruleId].examples}</Text>
-        </View>
+        <GrammarItem ruleId={ruleId} key={ruleId} />
       ))}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  wrapper: {
-    margin: 10,
-  },
-  textHighlight: {
-    color: settings.colors.primary.dark,
-    fontWeight: 'bold',
-    backgroundColor: settings.colors.secondary.light,
-  },
-  ruleTitle: {fontSize: 20, marginBottom: 5},
-  label: {
-    marginBottom: 20,
-  },
-  ruleText: {},
-  exampleText: {fontStyle: 'italic', marginTop: 5},
-});
-
-export const rules = {
-  FAQ_RULES_SCHAFT: {
-    label: <Text>-schaft : DIE</Text>,
-    text: (
-      <Text>
-        Wörter mit der Endung <Text style={styles.textHighlight}>-schaft </Text>
-        sind immer weiblich
-      </Text>
-    ),
-    examples: (
-      <Text>
-        Gemein<Text style={styles.textHighlight}>schaft</Text>, Bürg
-        <Text style={styles.textHighlight}>schaft</Text>, Freund
-        <Text style={styles.textHighlight}>schaft</Text>
-      </Text>
-    ),
-  },
-  FAQ_RULES_HEIT: {
-    label: <Text>-heit : DIE</Text>,
-    text: (
-      <Text>
-        Wörter mit der Endung <Text style={styles.textHighlight}>-heit </Text>
-        sind immer weiblich
-      </Text>
-    ),
-    examples: (
-      <Text>
-        Gemein<Text style={styles.textHighlight}>heit</Text>, Krank
-        <Text style={styles.textHighlight}>heit</Text>, Ein
-        <Text style={styles.textHighlight}>heit</Text>
-      </Text>
-    ),
-  },
-};
-
 export default FaqItems;
--- components/FaqScreen.tsx
+++ components/FaqScreen.tsx
@@ -1,49 +1,27 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 import sharedStyles from '../styles/sharedStyles';
-import settings from '../libs/settings.json';
 
-import UnicornCat from './SVGs/UnicornCat';
+import FaqUnicorn from './SVGs/FaqUnicorn';
 import FaqItems from './FaqItems';
 import {ScrollView} from 'react-native-gesture-handler';
 
 function FaqScreen() {
   return (
     <ScrollView>
-      <View style={[sharedStyles.screen, styles.wrapper]}>
-        <Text style={styles.title}>FAQ</Text>
+      <View style={sharedStyles.screen}>
+        <Text style={sharedStyles.screenTitle}>FAQ</Text>
+        <Text style={sharedStyles.screenSubTitle}>FAQ</Text>
 
-        <View style={styles.catWrapper}>
-          <UnicornCat width={150} />
+        <View style={sharedStyles.screenHeaderIcon}>
+          <FaqUnicorn width={150} />
         </View>
-        <View style={[sharedStyles.viewVertical, {padding: 0, margin: 10}]}>
+
+        <View style={sharedStyles.screenContent}>
           <FaqItems />
         </View>
       </View>
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  wrapper: {
-    paddingVertical: 20,
-  },
-  title: {
-    fontSize: 60,
-    fontWeight: 'bold',
-    color: 'black',
-    alignSelf: 'center',
-  },
-  catWrapper: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 1000,
-    backgroundColor: settings.colors.primary.background,
-    width: 250,
-    height: 250,
-    alignSelf: 'center',
-    marginTop: 20,
-  },
-});
 export default FaqScreen;
